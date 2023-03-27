89f2f1c5efe840f3402f64a322859c524b01a2df
aladham1 <63999566+aladham1@users.noreply.github.com>
2023-03-27 12:48:33 +0300

    Update services.php

--- config/services.php
+++ config/services.php
@@ -38,6 +38,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' =>  env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/login/google/callback'
+        'redirect' => 'http://quiz.bursacenneti.com/login/google/callback'
     ],
 ];
