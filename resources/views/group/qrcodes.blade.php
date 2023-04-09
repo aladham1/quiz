7ba26f556a25b633708b3d1808186cd25cdf435a
aladham1 <m.aladham@digitalfuture.sa>
2023-04-10 00:22:12 +0300

    Update qrcodes.blade.php

--- resources/views/group/qrcodes.blade.php
+++ resources/views/group/qrcodes.blade.php
@@ -6,14 +6,21 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-    @foreach($group->exams as $exam)
-        <div style="margin: auto; width: 200px;  margin-bottom: 40px">
-            <h3 style="color: #4181a7">{{$exam->title}}</h3>
-            <img src="data:image/png;base64,{!!  base64_encode(QrCode::format('png')->size(200)
-                    ->generate(route('exams.intro', ['exam' => $exam->id]))); !!}" alt=""></div>
-        <hr>
-    @endforeach
+<div class="container-fluid mt-4">
+    <div class="row">
+        @foreach($group->exams as $exam)
+            <div class="col-md-3 mb-3">
+                <h3 style="color: #4181a7">{{$exam->title}}</h3>
+                <img src="data:image/png;base64,{!!  base64_encode(QrCode::format('png')->size(200)
+                    ->generate(route('exams.intro', ['exam' => $exam->id]))); !!}" alt="">
+            </div>
+        @endforeach
+    </div>
+</div>
+
+
 </body>
 </html>
