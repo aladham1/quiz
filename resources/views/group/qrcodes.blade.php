b7ba6f905ed79e4d83c2008bc880dda96200ac57
aladham1 <63999566+aladham1@users.noreply.github.com>
2023-04-10 09:18:34 +0300

    Update qrcodes.blade.php

--- resources/views/group/qrcodes.blade.php
+++ resources/views/group/qrcodes.blade.php
@@ -6,11 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
 <div class="container-fluid mt-4">
     <div class="row text-center">
+        <div class="col-12">
+            <h3 class="text-center" style="color: #4181a7">{{$group->name}}</h3>
+        </div>
         @foreach($group->exams as $exam)
             <div class="col-3 mb-3">
                 <h3 style="color: #4181a7">{{$exam->title}}</h3>
@@ -21,6 +25,8 @@
     </div>
 </div>
 
-
+<script>
+    window.print();
+</script>
 </body>
 </html>
